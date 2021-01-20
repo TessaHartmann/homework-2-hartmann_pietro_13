8894f7b915783c30d9b041e108e033bd9dc06b42
ddp19b <77124628+ddp19b@users.noreply.github.com>
2021-01-20 13:59:34 -0500

    Update concat_primes.cpp

--- src/concat_primes.cpp
+++ src/concat_primes.cpp
@@ -19,47 +19,92 @@
 #include <assert.h>
 #include<string>
 
-/** \brief Brief function explaination
-   
-   And a long one...
+/*
+   \brief Brief function explaination
+
+   get_concatenated_primes:
+
+
+   get_slice_of_5:
+
+   The second function uses the concept and string function called substr, ie. substring.
+   "Get slice of 5" as it is named will aid in returning the 5 digits.
+   These 5 digits begin at the user entered index ("Please enter n = ") and will end after the 5th digit from that given index.
+   This is done using the parameters (index, 5) in "ret = primes.sub str(index, 5);".
+   Ret is then returned to print those 5 digits.
 
     @return std::string
 */
 
-std::string get_concatenated_primes()
+std:: string get_concatenated_primes()
 {
-    std::string concat_primes = "";
+    std :: string concat_primes = "";
     //Complete this function
 
-    return concat_primes.substr(0, 1000);
+    for(int i = 2; i <= 2000; i++)
+    {
+        bool prime = true;
+        for(int j = 2; j < i; j++)
+        {
+            if(i % j == 0 || i == j)
+            {
+                prime = false;
+                continue;
+            }
+        }
+        if(prime == true && i != 1)
+        {
+            concat_primes += std::to_string(i);
+        }
+    }
+    return concat_primes.substr(0,1000);
 }
 
-
-std::string get_slice_of_5(const std::string & primes, const int index)
+std:: string get_slice_of_5(const std:: string &primes, const int index)
 {
-    std::string ret = "";
+    std:: string ret = "";
+
     //Complete this function
+    ret = primes.substr(index, 5);
+
     return ret;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     using namespace std;
     int n;
     string concat_primes;
-    if (argc >= 2){
+
+    if (argc >= 2)
+    {
         concat_primes = get_concatenated_primes();
+
         assert(get_slice_of_5(concat_primes, 3) == "71113");
         cout<<"Unit Test 1 passed\n";
         assert(get_slice_of_5(concat_primes, 6) == "13171");
         cout<<"Unit Test 2 passed\n";
-        
+
         //Please add 3 more unit test here/
-    } else{
+        assert(get_slice_of_5(concat_primes, 9) == "71923");
+        cout<<"Unit Test 3 passed\n";
+
+        assert(get_slice_of_5(concat_primes, 130) == "25125");
+        cout<<"Unit Test 4 passed\n";
+
+        assert(get_slice_of_5(concat_primes, 998) == "91");
+        cout<<"Unit Test 5 passed\n";
+    }
+    else
+    {
         cout<< "Please input n: ";
-        while(std::cin >> n)
+
+        while(std:: cin >> n)
         {
             concat_primes = get_concatenated_primes();
+
             std::cout << get_slice_of_5(concat_primes, n) << std::endl;
+
             cout<< "Please input n: ";
         }
     }
